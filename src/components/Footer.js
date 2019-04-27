fb088903f67683f31718df67e4801ce6d86a272c
Chris Troutner <chris.troutner@gmail.com>
2019-04-27 16:03:15 -0700

    Removed eslint warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react'
 
 const Footer = (props) => (
--- src/pages/elements.js
+++ src/pages/elements.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
