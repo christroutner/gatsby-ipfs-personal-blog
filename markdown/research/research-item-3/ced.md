d78c6081caf2c0d6a8b8058a9cf7b2fe4f8fc8c4
Chris Troutner <chris.troutner@gmail.com>
2019-05-04 07:56:09 -0700

    Wrapped markdown generated pages in Layout component

--- markdown/research/research-item-3/ced.md
+++ markdown/research/research-item-3/ced.md
@@ -5,5 +5,4 @@ parent: "Research Item 3"
 path: "/research/research-item-3/fusce"
 ---
 
-# Fusce
 Fusce tristique quis libero nec luctus. Aliquam viverra congue laoreet. Etiam ut malesuada sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam erat volutpat. Aliquam at sem viverra, consectetur enim quis, vehicula risus. Etiam dignissim diam leo, sit amet fringilla mi posuere nec. Nullam sit amet facilisis ipsum.
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -7,6 +7,13 @@
 
 import React from "react"
 import { graphql } from "gatsby"
+import styled from 'styled-components'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+
+const BlogPostContainer = styled.div`
+  padding: 25px;
+`
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -14,16 +21,23 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
+    <Layout>
+      <Helmet>
+        <title>{frontmatter.title}</title>
+        <meta name="description" content="Markdown Generated Page" />
+      </Helmet>
+
+      <BlogPostContainer className="blog-post-container">
+        <div className="blog-post">
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
+      </BlogPostContainer>
+    </Layout>
   )
 }
 
